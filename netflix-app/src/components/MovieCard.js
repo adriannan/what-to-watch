1327c70da1a2fbbfefba7393abc89ce7a9161b1a
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-02-23 14:12:59 +0100

    style movie cards

--- netflix-app/src/components/MovieCard.js
+++ netflix-app/src/components/MovieCard.js
@@ -4,14 +4,18 @@ import "../styles/components.scss";
 const MovieCard = ({ title, image, relased, runtime, rating }) => {
   return (
     <div className="movie__card">
-      <img src={image} alt="movie"></img>
-      <h3 className="movie__tit">{title}</h3>
-      <p className="movie__year">{relased}</p>
-      <p className="movie__time">{runtime}</p>
-      <p className="movie__rate">
-        <i class="material-icons">star</i>
-        {rating}
-      </p>
+      <div className="movie__img">
+        <img src={image} alt="movie"></img>
+      </div>
+      <div className="movie__info">
+        <h3 className="movie__tit">{title}</h3>
+        <p className="movie__year">{relased}</p>
+        <p className="movie__time">{runtime}</p>
+        <p className="movie__rate">
+          <i class="material-icons">star</i>
+          {Math.round(rating * 10) / 10}
+        </p>
+      </div>
     </div>
   );
 };
