b7d5356a3a2cf9ae2024315015710245c9bde1c8
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-03-09 21:28:41 +0100

    add series

--- netflix-app/src/pages/Shows.js
+++ netflix-app/src/pages/Shows.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/pages.scss";
+import MovieCard from "../components/MovieCard";
+import movies from "../netflix-api/movies.json";
+
+let max = 6;
 
 const Shows = () => {
+  const moviesList = Object.values(movies);
+  const seriesSort = moviesList.filter(item => item.type === "series");
+  const series = seriesSort.map(show => (
+    <MovieCard id={show.netflixid} {...show} />
+  ));
+  const [seriesPart, addSeries] = useState(series.slice(0, 6));
+  function showMore() {
+    max += 6;
+    addSeries(series.slice(0, max));
+  }
   return (
     <section className="section__tvshows">
       <div className="tvshows__header">
@@ -24,6 +38,10 @@ const Shows = () => {
           <i class="material-icons tvshows__btn-icon">arrow_drop_down</i>
         </button>
       </div>
+      <div className="movies__main">{seriesPart}</div>
+      <button className="movies__btn-more" onClick={showMore}>
+        Show More
+      </button>
     </section>
   );
 };
